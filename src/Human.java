163d3089b0e3631eb5a666e3c5d6304eb8a04138
nikitamavrodiy <mna1998@mail.ru>
2022-10-18 17:42:14 +0300

    Homework are finally completed

--- src/Human.java
+++ src/Human.java
@@ -1,17 +1,37 @@
-public class Human {
-    String name;
-    String town;
-    int yearOfBirth;
-    String job;
+import java.util.Calendar;
 
-    Human(String name, String town, int yearOfBirth, String job) {
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town +
-                ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+public class Human {
+    private String name;
+    private String town;
+    private int yearOfBirth;
+    private String job;
+
+    Human(int age) {
+        this("(Информация не указана)", "(Информация не указана)", age, "(Информация не указана)");
     }
+
+    Human(String name, String town, int age, String job) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Значение возраста не может быть отрицательным!");
+        }
+        this.yearOfBirth = Calendar.getInstance().get(Calendar.YEAR) - age;
+        this.name = checkInputField(name);
+        this.town = checkInputField(town);
+        this.job = checkInputField(job);
+        System.out.println(this.toString());
+    }
+
+    String checkInputField(String check) {
+        return check = (check.length() > 0) ? check : "(Информация не указана)";
+    }
+
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town +
+                ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!";
+    }
+
+
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,18 @@ public class Main {
         Human artem = new Human("Артем", "Москва", 27, "директор по развитию бизнеса");
 
         System.out.println();
+        Human Karlos = new Human(27);
+        Human unnamed = new Human("","",0,"");
+
+        System.out.println();
         Car car1 = new Car("Lada", "Granta", 2015, "Россия", "желтый", 1.7);
         Car car2 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0);
         Car car3 = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
         Car car4 = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красный", 2.4);
         Car car5 = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6);
+
+        System.out.println();
+        Car carDefault = new Car("", "Avante", "Южная Корея", "оранжевый");
 
     }
 
